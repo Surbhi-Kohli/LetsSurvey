c720f2aede7f53639357a5c97f39d18c4da8fbe4
surbhikohli <surbhikohli1995@gmail.com>
2021-01-06 19:40:17 +0530

    changed browser router to hashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
+import {HashRouter as Router,Switch,Route} from "react-router-dom";
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import thunk from "redux-thunk";
