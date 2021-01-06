7ff356da0a56d25631cfa86f64dcad97c163da75
surbhikohli <surbhikohli1995@gmail.com>
2021-01-06 17:07:54 +0530

    gitignore

---
+++ build/precache-manifest.0d0036ba3da37e438a909e176a4c2609.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78fea98b016f002fc8382c8d8174c9bf",
+    "url": "/LetsSurvey/index.html"
+  },
+  {
+    "revision": "71563874f672c4e80fd1",
+    "url": "/LetsSurvey/static/css/2.8c5002dc.chunk.css"
+  },
+  {
+    "revision": "60d7589c529937dc0e55",
+    "url": "/LetsSurvey/static/css/main.7ecd9a48.chunk.css"
+  },
+  {
+    "revision": "71563874f672c4e80fd1",
+    "url": "/LetsSurvey/static/js/2.7073b50b.chunk.js"
+  },
+  {
+    "revision": "4e2eba761852db3e0ede28046b7ba53c",
+    "url": "/LetsSurvey/static/js/2.7073b50b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60d7589c529937dc0e55",
+    "url": "/LetsSurvey/static/js/main.3a9f1b4e.chunk.js"
+  },
+  {
+    "revision": "27c4a7f49d4de928e066",
+    "url": "/LetsSurvey/static/js/runtime-main.04d1c63f.js"
+  },
+  {
+    "revision": "1f3149970c14b538967a39046e5e075f",
+    "url": "/LetsSurvey/static/media/banner2.1f314997.jpg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/LetsSurvey/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/LetsSurvey/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/LetsSurvey/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/LetsSurvey/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/LetsSurvey/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/LetsSurvey/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/LetsSurvey/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/LetsSurvey/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/LetsSurvey/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/LetsSurvey/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/LetsSurvey/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/LetsSurvey/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/LetsSurvey/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/LetsSurvey/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/LetsSurvey/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/LetsSurvey/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
